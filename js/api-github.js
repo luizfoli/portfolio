02bff649b6836941a42afb2740443391cba61838
Luiz Oliveira <oliveira.lfo@gmail.com>
2019-09-13 22:14:02 -0300

    Finalizando a visualização de informações do github

--- js/api-github.js
+++ js/api-github.js
@@ -1,9 +1,34 @@
-async function createDivProjects() {
+async function createDivProject() {
     const repos = await getRepos();
     const containerProjects = document.querySelector('#containerProjects');
 
     repos.map((repo) => {
+
+        const { name, html_url, description, language } = repo;
+
         const newDivProject = document.createElement('div');
+        const newHeader = document.createElement('header');
+        const newLinkHeader = document.createElement('a');
+        const newArticle = document.createElement('article');
+
+        const repoDescription = document.createElement('p');
+        repoDescription.textContent = description;
+
+        const repoLanguage = document.createElement('p');
+        repoLanguage.textContent = language;
+
+        newArticle.appendChild(repoDescription);
+        newArticle.appendChild(repoLanguage);
+
+        newLinkHeader.textContent = name;
+        newLinkHeader.target = '_blank';
+        newLinkHeader.href = html_url;
+
+        newHeader.appendChild(newLinkHeader);
+        newDivProject.appendChild(newHeader);
+        newDivProject.appendChild(newArticle);
+
+
         newDivProject.classList.add('project');
         containerProjects.appendChild(newDivProject);
     })
@@ -17,4 +42,4 @@ async function getRepos() {
     return repos.filter((repo) => (!repo.fork));
 }
 
-createDivProjects();
+createDivProject();
